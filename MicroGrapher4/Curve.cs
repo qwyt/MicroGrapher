32ee38a904f413f26bd63f1976ae4c24c197b9fb
paulius <paulius_puodziunas@fmail.co.uk>
2014-04-15 21:36:54 +0300

    Version 1.01

---
+++ MicroGrapher4/Curve.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace MicroGrapher4
+{
+    class Curve
+        /*
+         *  Represents the graph on the canvas
+         * 
+         * 
+         */ 
+    {
+        public float start;
+        public float end;
+        public Point[] points;
+        Canvas canvas;
+
+
+
+     
+
+    }
+}
---
+++ MicroGrapher4/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MicroGrapher4
+{
+    class Graph
+        /*
+         *  Represents the abstract graph object
+         * 
+         * 
+         */
+    {
+        private FunctionRepr _function;
+
+        public string Function { set { _function = new FunctionRepr(value); } }
+
+        public Graph(string function)
+        {
+            Function = function;
+            Function = function;
+        }
+
+
+        public Point[] GetPoints(double from = 0d, double to = 10d, int count = 10)
+        {
+            var output = new Point[count];
+            double iterator = (to - from)/count;
+            double value = from;
+            for (int i = 0; i < count; i++, value+=iterator)
+            {
+                output[i] = new Point(value, _function.GetValueAtPoint( value) );
+            }
+            return output;
+        }
+        public override string ToString()
+        {
+            return _function.ToString();
+        }
+
+    }
+}
